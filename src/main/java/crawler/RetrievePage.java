c7fe106aa04de4f1a40bb5c227422f46836a3381
zhangren <xueluowuhen.2007@163.com>
2017-01-20 10:48:08 +0800

    add trie tree

---
+++ src/main/java/crawler/RetrievePage.java
@@ -0,0 +1,39 @@
+package crawler;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by magneto on 17-1-16.
+ */
+public class RetrievePage {
+    public static String downloadPage(String path) throws IOException {
+        //根据传入的路径构造URL
+        URL pageURL = new URL(path);
+        //创建网络流
+        BufferedReader reader = new BufferedReader(new InputStreamReader(pageURL.openStream()));
+        String line;
+        //读取网页内容
+        StringBuilder pageBuffer = new StringBuilder();
+        while ((line = reader.readLine())  != null) {
+            pageBuffer.append(line);
+        }
+
+        return pageBuffer.toString();
+    }
+
+    public static void main(String[] args) {
+        //抓取lietu首页然后输出
+        //String path = "http://www.lietu.com";
+        String path = "http://www.baidu.com";
+        try {
+            System.out.println(RetrievePage.downloadPage(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/crawler/TrieNode.java
@@ -0,0 +1,22 @@
+package crawler;
+
+import java.util.HashMap;
+
+/**
+ * Created by magneto on 17-1-17.
+ */
+public class TrieNode {
+    char c;
+    boolean isLeaf;
+    HashMap<Character, TrieNode> children = new HashMap<Character, TrieNode>();
+
+    public TrieNode(){}
+    public TrieNode(char c) {
+        this.c = c;
+    }
+
+    public static void main(String[] args) {
+        TrieNode t = new TrieNode();
+        System.out.println(t.isLeaf);
+    }
+}
